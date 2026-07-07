92208cee067ff12b64cb7ce8b959e0d8d3a76bb9
BhadmusX <bhadmusalameen052007@gmail.com>
2026-07-07 10:19:19 +0100

    pascal completed

--- computer_science/recursion/5_pascal/pascal.js
+++ computer_science/recursion/5_pascal/pascal.js
@@ -1,5 +1,15 @@
-const pascal = function() {
-  
+const pascal = function(n) {
+  if(n===1){
+    return [1];
+  }
+  const prev = pascal(n-1);
+  const shifright = [0, ...prev];
+  const shiftleft = [...prev, 0];
+
+ const result =  shifright.map((value, index)=> {
+    return value + shiftleft[index];
+  })
+  return result;
 };
   
 // Do not edit below this line
